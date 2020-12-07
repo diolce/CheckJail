b4975e27ccdaf3d762b772b3921eae17a1e9bffb
Diego Olmo <dolmocejudo@gmail.com>
2020-12-07 23:22:04 +0100

    Initial Commit

---
+++ CheckJail/CheckJail.h
@@ -0,0 +1,18 @@
+//
+//  CheckJail.h
+//  CheckJail
+//
+//  Created by Diego Olmo Cejudo on 7/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckJail.
+FOUNDATION_EXPORT double CheckJailVersionNumber;
+
+//! Project version string for CheckJail.
+FOUNDATION_EXPORT const unsigned char CheckJailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckJail/PublicHeader.h>
+
+
